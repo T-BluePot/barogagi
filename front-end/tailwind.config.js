653bb8ebf7e229f3c91b4eda36c84f50c8bd0144
정은우 <136785138+Jung-eunwoo@users.noreply.github.com>
2025-06-14 17:37:13 +0900

    [Design&Init] CSS 기본 세팅 설정 (#1)
    * comment: index html 문구 일부 수정정
    
    * chore: tailwind css 라이브러리 설치
    
    * chore: tailwindCSS 4.0 설치 및 기본 색상 변수 정의

---
+++ front-end/tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        main: "#BAFD4F",
+        "main-dark": "#8ED71B",
+        "main-disable": "#D6FF96",
+        "alert-red": "#FF3B38",
+      },
+      fontFamily: {
+        sans: ["Pretendard Variable", "sans-serif"],
+      },
+      fontSize: {
+        header: ["28px", { lineHeight: "34px", letterSpacing: "-2.5%" }],
+        "title-01": ["24px", { lineHeight: "30px", letterSpacing: "-2.5%" }],
+        "title-02": ["20px", { lineHeight: "25px", letterSpacing: "-2.5%" }],
+        subtitle: ["16px", { lineHeight: "21px", letterSpacing: "-2.5%" }],
+        body: ["16px", { lineHeight: "21px", letterSpacing: "-2.5%" }],
+        caption: ["14px", { lineHeight: "19px", letterSpacing: "-2.5%" }],
+        description: ["12px", { lineHeight: "16px", letterSpacing: "-2.5%" }],
+      },
+      spacing: {
+        "screen-padding": "24px",
+        "header-bar": "60px",
+        "footer-bar": "56px",
+        "button-height": "56px",
+        "gap-section": "32px",
+        "gap-sm": "8px",
+        "gap-md": "16px",
+      },
+      borderRadius: {
+        card: "0.75rem",
+        button: "9999px",
+      },
+    },
+  },
+  plugins: [],
+};
